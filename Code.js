43896031916b012653007fe9b0900b0b52572bff
Daniel <dbuehler1@my.wctc.edu>
2020-10-21 15:27:11 -0500

    Initial commit

---
+++ Code.js
@@ -0,0 +1,15 @@
+var count = 0;
+function resetCount()
+{
+    count = 0;
+}
+function counting()
+{
+    ++count;
+}
+function alertCount()
+{
+
+    alert("You clicked: " + count + " times");
+}
+
